446005656940cc42bf4b9da8cccfc2d923ec2775
zain2323 <zainsiddiqui2323@gmail.com>
2024-12-08 11:38:54 +0500

    handling invalid commands

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    input()
+    command = input()
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
